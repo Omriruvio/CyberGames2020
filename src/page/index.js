3812576dff1f35993f1a0e7645bce08bba932d19
Omri Ruvio <omriruvio@gmail.com>
2022-03-06 06:55:30 +0200

    style(video): add posters

--- src/page/index.js
+++ src/page/index.js
@@ -1,6 +1,10 @@
 import './index.css'
 const trailerVideo = new URL('../videos/CyberGamesTrailer.mp4', import.meta.url);
 const rupinVideo = new URL('../videos/CyberGamesinRupincollege.mp4', import.meta.url);
+const trailerPoster = new URL('../images/videopostertrailer.png', import.meta.url);
+const rupinPoster = new URL('../images/videoposterrupin.png', import.meta.url);
 
 document.querySelector('#cybergamestrailer').src = trailerVideo;
+document.querySelector('#cybergamestrailer').poster = trailerPoster;
 document.querySelector('#cybergamesrupin').src = rupinVideo;
+document.querySelector('#cybergamesrupin').poster = rupinPoster;
