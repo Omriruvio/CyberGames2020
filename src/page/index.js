cce890b360707bab48d392a9159825ee1ce0cd34
Omri Ruvio <omriruvio@gmail.com>
2022-04-23 09:45:36 +0300

    feature: replace video
    change styles for videos and features

--- src/page/index.js
+++ src/page/index.js
@@ -1,23 +1,40 @@
-import './index.css'
+import './index.css';
 
 const teamInfo = {
-  omri: {name:'Omri Ruvio', role:'Co-founder / CEO',about: 'Full stack web developer, cyber enthusiast, L33T gamer, leader of virtual and real-life teams.'},
-  guy: {name:'Guy Ruvio', role:'Co-founder', about:'co-founder of Towersec (acq. by Harman / Samsung), Ex 8200, high security positions in governmental organizations conductive security assessments of high-profile civil organizations and Ethical hacker.'},
-  shai: {name:'Shai Kavas', role:'Advisory Board',about: 'Cyber Security Veteran. RAD, Intel. CEO of CyberGuild Ventures, Investor and Venture Builder.'},
-  saar: {name:'Saar Dickman', role:'Advisory Board',about: 'Co-founder of Towersec (acq. by Harman / Samsung), Founder and CEO of Dynamicinfrastructure'}
-}
+  omri: {
+    name: 'Omri Ruvio',
+    role: 'Co-founder / CEO',
+    about: 'Full stack web developer, cyber enthusiast, L33T gamer, leader of virtual and real-life teams.',
+  },
+  guy: {
+    name: 'Guy Ruvio',
+    role: 'Co-founder',
+    about:
+      'co-founder of Towersec (acq. by Harman / Samsung), Ex 8200, high security positions in governmental organizations conductive security assessments of high-profile civil organizations and Ethical hacker.',
+  },
+  shai: {
+    name: 'Shai Kavas',
+    role: 'Advisory Board',
+    about: 'Cyber Security Veteran. RAD, Intel. CEO of CyberGuild Ventures, Investor and Venture Builder.',
+  },
+  saar: {
+    name: 'Saar Dickman',
+    role: 'Advisory Board',
+    about: 'Co-founder of Towersec (acq. by Harman / Samsung), Founder and CEO of Dynamicinfrastructure',
+  },
+};
 
 const trailerVideo = new URL('../videos/CyberGamesTrailer.mp4', import.meta.url);
-const rupinVideo = new URL('../videos/CyberGamesinRupincollege.mp4', import.meta.url);
+const rupinVideo = new URL('../videos/CyberGamesRupinCensored.mp4', import.meta.url);
 const trailerPoster = new URL('../images/videopostertrailer.png', import.meta.url);
 const rupinPoster = new URL('../images/videoposterrupin.png', import.meta.url);
-const teamMemberNameElement = document.querySelector('#js-team-member-name')
-const teamMemberRoleElement = document.querySelector('#js-team-member-role')
-const teamMemberDescriptionElement = document.querySelector('#js-team-member-description')
-const omriIcon = document.querySelector('#js-omri-icon')
-const guyIcon = document.querySelector('#js-guy-icon')
-const shaiIcon = document.querySelector('#js-shai-icon')
-const saarIcon = document.querySelector('#js-saar-icon')
+const teamMemberNameElement = document.querySelector('#js-team-member-name');
+const teamMemberRoleElement = document.querySelector('#js-team-member-role');
+const teamMemberDescriptionElement = document.querySelector('#js-team-member-description');
+const omriIcon = document.querySelector('#js-omri-icon');
+const guyIcon = document.querySelector('#js-guy-icon');
+const shaiIcon = document.querySelector('#js-shai-icon');
+const saarIcon = document.querySelector('#js-saar-icon');
 
 document.querySelector('#cybergamestrailer').src = trailerVideo;
 document.querySelector('#cybergamestrailer').poster = trailerPoster;
@@ -29,7 +46,7 @@ guyIcon.addEventListener('mouseenter', () => renderTeamInfo(teamInfo.guy));
 shaiIcon.addEventListener('mouseenter', () => renderTeamInfo(teamInfo.shai));
 saarIcon.addEventListener('mouseenter', () => renderTeamInfo(teamInfo.saar));
 
-function renderTeamInfo (member) {
+function renderTeamInfo(member) {
   teamMemberNameElement.textContent = member.name;
   teamMemberRoleElement.textContent = member.role;
   teamMemberDescriptionElement.textContent = member.about;
